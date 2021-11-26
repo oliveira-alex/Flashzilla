06ad9dff3652195031de762dda526b1fa4d58cbc
ALeX <alexdeoliveira.poli@gmail.com>
2021-11-26 17:19:35 -0300

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Alex Oliveira on 26/11/21.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
