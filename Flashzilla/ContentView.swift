3db60354b8060d88bf95e12c0e2696a343db951a
ALeX <alexdeoliveira.poli@gmail.com>
2021-11-27 17:43:46 -0300

    Making vibrations with Core Haptics – Flashzilla SwiftUI Tutorial 2/15 (UIKit's Haptics)

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -8,36 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var offset = CGSize.zero
-    @State private var isDragging = false
-    
     var body: some View {
-        let dragGesture = DragGesture()
-            .onChanged { value in
-                self.offset = value.translation
-            }
-            .onEnded { _ in
-                withAnimation {
-                    self.offset = .zero
-                    self.isDragging = false
-                }
-            }
-        
-        let pressGesture = LongPressGesture(minimumDuration: 0.3)
-            .onEnded { value in
-                withAnimation {
-                    self.isDragging = true
-                }
-            }
-        
-        let combinedGesture = pressGesture.sequenced(before: dragGesture)
-        
-        return Circle()
-            .fill(Color.red)
-            .frame(width: 64, height: 64)
-            .scaleEffect(isDragging ? 1.5 : 1)
-            .offset(offset)
-            .gesture(combinedGesture)
+        Text("Hello, World!")
+            .padding()
+            .onTapGesture(perform: simpleSuccess)
+    }
+    
+    func simpleSuccess() {
+        let generator = UINotificationFeedbackGenerator()
+        generator.notificationOccurred(.success)
     }
 }
 
