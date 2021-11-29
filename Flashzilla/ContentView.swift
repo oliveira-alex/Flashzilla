f37a4119e8a1ee093e476f75516547848a63f0b6
ALeX <alexdeoliveira.poli@gmail.com>
2021-11-29 09:52:11 -0300

    Supporting specific accessibility needs with SwiftUI – Flashzilla SwiftUI Tutorial 6/15 (Reduce transparency)

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -7,32 +7,15 @@
 
 import SwiftUI
 
-func withOptionalAnimation<Result>(_ animation: Animation? = .default, _ body: () throws -> Result) rethrows -> Result {
-    if UIAccessibility.isReduceMotionEnabled {
-        return try body()
-    } else {
-        return try withAnimation(animation, body)
-    }
-}
-
 struct ContentView: View {
-//    @Environment(\.accessibilityReduceMotion) var reduceMotion
-    @State private var scale: CGFloat = 1
+    @Environment(\.accessibilityReduceTransparency) var reduceTransparency
     
     var body: some View {
         Text("Hello, World!")
-            .padding()
-            .scaleEffect(scale)
-            .onTapGesture {
-//                if reduceMotion {
-//                    scale *= 1.5
-//                } else {
-//                    withAnimation {
-                    withOptionalAnimation {
-                        scale *= 1.5
-                    }
-//                }
-            }
+            .padding(50)
+            .background(reduceTransparency ? Color.black : Color.black.opacity(0.5))
+            .foregroundColor(Color.white)
+            .clipShape(Circle())
     }
 }
 
